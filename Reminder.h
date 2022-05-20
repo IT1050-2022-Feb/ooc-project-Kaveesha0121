05676c6243a9d2640b16287b5bde450cfb2973ac
Kaveesha0121 <88632063+Kaveesha0121@users.noreply.github.com>
2022-05-20 20:30:42 +0530

    IT21267468_MLB_04_02_08
    Individual part
    Reminder class

---
+++ Reminder.h
@@ -0,0 +1,13 @@
+class Reminder
+{
+private:
+  int userID;
+  char ReminderTopic[50];
+  char AboutReminder[1000];
+
+public:
+  Reminder();
+  Reminder(int userID, char Topic[], char About[]);
+  char SendReminder(int userID, char Topic[], char About[]);
+  ~ Reminder ();
+};
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Reminder.h"
+using namespace std;
+
+int main() {
+  cout << "Hello World!\n";
+  
+}
